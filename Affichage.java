c9e538ce49b28af3d3ad2d87272614b5f7f2f8e0
Linou92 <lina_abuhijleh@hotmail.fr>
2016-12-08 23:57:35 +0100

    Add files via upload

--- Affichage.java
+++ Affichage.java
@@ -1,5 +1,7 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -9,23 +11,31 @@ import javax.swing.JPanel;
 
 public class Affichage extends JFrame{
 	
-	private JPanel panel = new JPanel();
-	private JLabel label = new JLabel();
-    private JButton bouton1 = new JButton("Start");
-    private int compt = Bouton.getCompt();
+	private JPanel background = new JPanel();
+	private static JLabel affichage = new JLabel();
     
     public Affichage(){
-    	panel.setLayout(new BorderLayout());
-    	this.setTitle("deuxième fenetre");
-    	this.setSize(300,300);
-    	this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-    	this.setLocationRelativeTo(null);
  
-    	panel.setBackground(Color.MAGENTA);
-    	label.setText("Vous avez cliqué " +compt+ " fois");
-  
-    	panel.add(label);   
-    	setContentPane(panel);
+    	this.setTitle("Compteur");
+    	this.setPreferredSize(new Dimension(500,500));
+    	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	this.setLocation(800,400);
+    	this.setResizable(true);
     	this.setVisible(true);    
+    	this.pack();
+    	
+    	Font font = new Font("Vivaldi", Font.BOLD,40);
+    	affichage.setFont(font);
+    	affichage.setHorizontalAlignment(JLabel.CENTER);
+    	affichage.setVerticalAlignment(JLabel.CENTER);
+    	affichage.setText("Vous avez cliqué 0 fois !");
+    	
+    	background.setBackground(Color.PINK);
+    	background.add(affichage);   
+    	setContentPane(background);
+    }
+    
+    public static JLabel getJLabel(){
+    	return affichage;
     }
 }
--- Bouton.java
+++ Bouton.java
@@ -1,35 +1,46 @@
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+
 import java.awt.*;
 import java.awt.event.*;
+
 import javax.swing.*;
 
 public class Bouton extends JFrame implements ActionListener{
 	
-	private Affichage fen2;
-	private JButton clique = new JButton("CLiquez!");
-	private JPanel panel = new JPanel();
+	private JButton clique = new JButton("Cliquez !");
+	private JPanel background = new JPanel();
 	private static int compt=0;
-	
+	private JLabel affichage = Affichage.getJLabel();
+
 	public Bouton(){
-		panel.setLayout(null);
-		this.setSize(800,800);
+
 		this.setTitle("Bouton");
+		this.setPreferredSize(new Dimension(500,500));
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setLocationRelativeTo(null);
-		
-		clique.setBackground(Color.cyan);
-		clique.setBounds(200,150,300,200);
-		
-		panel.add(clique);
-		setContentPane(panel);
-		clique.addActionListener(this);
+		//this.setLocationRelativeTo(null);
+		this.setLocation(500,400);
+		this.setSize(500, 500);
 		this.setVisible(true);
+		this.pack();
+		
+		clique.setBackground(Color.MAGENTA);
+		clique.setForeground(Color.WHITE);
+		Font font = new Font("Vivaldi", Font.BOLD,40);
+    	clique.setFont(font);
+    	clique.addActionListener(this);
+    	
+    	background.setBackground(Color.PINK);
+		background.add(clique,BorderLayout.CENTER);
+		setContentPane(background);
 	}
 	
 	public static int getCompt() {
@@ -37,12 +48,7 @@ public class Bouton extends JFrame implements ActionListener{
 	}
 	
 	public void actionPerformed(ActionEvent e){
-		//this.dispose();
 		this.compt++;
-		fen2 = new Affichage();
-	}
-	
-	public static void main(String[] args){
-		new Bouton();
+		affichage.setText("Vous avez cliqué " +compt+ " fois !");
 	}
 }
